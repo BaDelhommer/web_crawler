2bd040fe9f857a3a819ebbf60d9168ae4cfeaac1
BaDelhommer <badelhommer@gmail.com>
2024-05-20 18:19:45 -0500

    completed getURLsFromHTML and added tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+import { JSDOM } from 'jsdom'
+
 function normalizeURL(url) {
     const urlObj = new URL(url);
     let fullPath = `${urlObj.host}${urlObj.pathname}`
@@ -7,4 +9,26 @@ function normalizeURL(url) {
     return fullPath
 };
 
-export { normalizeURL };
+function getURLsFromHTML(html, baseURL) {
+    const urls = []
+    const dom = new JSDOM(html)
+    const anchors = dom.window.document.querySelectorAll('a')
+  
+    for (const anchor of anchors) {
+      if (anchor.hasAttribute('href')) {
+        let href = anchor.getAttribute('href')
+  
+        try {
+          // convert any relative URLs to absolute URLs
+          href = new URL(href, baseURL).href
+          urls.push(href)
+        } catch(err) {
+          console.log(`${err.message}: ${href}`)
+        }
+      }
+    }
+  
+    return urls
+  }
+
+export { normalizeURL, getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,6 @@
 import { test, expect } from "@jest/globals"
-import { normalizeURL } from "./crawl.js"
+import { normalizeURL, getURLsFromHTML } from "./crawl.js"
+import { JSDOM } from 'jsdom'
 
 test('normalizeURL protocol', () => {
     const input = 'https://blog.boot.dev/path'
@@ -26,5 +27,49 @@ test('normalizeURL http', () => {
     const input = 'http://blog.boot.dev/path'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML len', () => {
+    const sampleHTML = `
+    <html>
+        <body>
+        <a href="https://boot.dev">boot.dev</a>
+        <a href="/blog">Blog</a>
+        <a href="https://example.com">Example</a>
+        </body>
+    </html>
+    `
+    const baseURL = 'https://boot.dev'
+    const actual = getURLsFromHTML(sampleHTML, baseURL)
+    const expected = ["https://boot.dev/", "https://boot.dev/blog", "https://example.com/"]
+    console.log(actual)
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML no_links', () => {
+    const sampleHTML = `
+    <html>
+        <body>
+        <p>No links here</p>
+        </body>
+    </html>
+    `
+    const baseURL = 'https://boot.dev'
+    const actual = getURLsFromHTML(sampleHTML, baseURL)
+    const expected = []
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML malformed_html', () => {
+    const sampleHTML = `
+    <html>
+        <body>
+        <a href="https://boot.dev">boot.dev</a>
+        <a href="/blog">Blog</a>
+    `
+    const baseURL = 'https://boot.dev'
+    const actual = getURLsFromHTML(sampleHTML, baseURL)
+    const expected = ["https://boot.dev/", "https://boot.dev/blog"]
     expect(actual).toEqual(expected)
 })
